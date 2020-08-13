b3e54cbe89e1dc833a889b560c5e6fc9163ce7c5
Сергей <nyos@rambler.ru>
2020-08-13 16:26:30 +0500

    0.0.3

---
+++ class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+класс модуля
+**/
+
+namespace Nyos\mod;
+
+// if (!defined('IN_NYOS_PROJECT'))
+//    throw new \Exception('Сработала защита от розовых хакеров, обратитесь к администрратору');
+
+class Bibi {
+
+    public static $dir_img_server = false;
+
+    public static function start() {
+        /**
+        
+        **/
+    }
+
+}
